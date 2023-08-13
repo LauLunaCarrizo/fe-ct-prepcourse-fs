3c0b7823fae902c7b29ee5d4b60202c30a39eae3
lauu <lautaropp03@outlook.com>
2023-08-13 20:08:00 -0300

    miTarea

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -3,29 +3,57 @@
 
 function sonIguales(x, y) {
    // Retorna true si "x" e "y" son iguales.
+   if(x == y){
+      return ("true");
+   }
+   else if (x != y){
+      return ("false");
+   }
    // De lo contrario, retorna false.
    // Tu código:
 }
 
 function tienenMismaLongitud(str1, str2) {
+   if (str1 == str2){
+      return ("true");
+   }
+   else if (str1 != str2){
+      return ("false")
+   }
+
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
    // Tu código:
 }
 
 function menosQueNoventa(num) {
+   if (num<90){
+
+      return("true");
+   }
+   else if (num>=90) {
+
+      return ("false");
+   }
    // Retorna true si el argumento "num" es menor que noventa.
    // De lo contrario, retorna false.
    // Tu código:
 }
 
 function mayorQueCincuenta(num) {
+   if (num>50){
+      return("true");
+   }
+   else {
+      return("false");
+   }
    // Retorna true si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna false.
    // Tu código:
 }
 
 function esPar(num) {
+   
    // Retorna true si "num" es par.
    // De lo contrario, retorna false.
    // Tu código:
--- M03 JavaScript Fundamentos/Ejercicio 04.js
+++ M03 JavaScript Fundamentos/Ejercicio 04.js
@@ -4,31 +4,43 @@
 // ⛔️ Recuerda que debes utilizar el objeto global "Math".
 
 function elevarAlCuadrado(num) {
+   var potencia = Math.pow(num,2);
+   return (potencia);
    // Retorna el valor de "num" elevado al cuadrado.
    // Tu código:
 }
 
 function elevarAlCubo(num) {
+   var potencia = Math.pow(num,3);
+   return (potencia);
    // Retorna el valor de "num" elevado al cubo.
    // Tu código:
 }
 
 function elevar(num, exponent) {
+   var potencia = Math.pow(num,exponent);
+   return (potencia);
    // Retorna el valor de "num" elevado al exponente "exponent".
    // Tu código:
 }
 
 function redondearNumero(num) {
+   var redondeo = Math.round(num);
+   return (redondeo);
    // Redondea "num" al entero más próximo y retórnalo.
    // Tu código:
 }
 
 function redondearHaciaArriba(num) {
+   var redondeo= Math.ceil(num);
+   return (redondeo);
    // Redondea "num" hacia arriba y retórnalo.
    // Tu código:
 }
 
 function numeroRandom() {
+   var potencia = Math.random();
+   return (potencia);
    // Genera un número al azar entre 0 y 1 y retórnalo.
    // Tu código:
 }
